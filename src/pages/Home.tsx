7f3218a393721901f2eb394a400fe6a04319bb9f
Sergio <S.suarerez@gmail.com>
2020-09-22 08:57:40 -0500

    :gun: Remove product section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,11 @@
 import React from "react";
 
 import Landing from "../components/Landing/Landing";
-import About from "../components/About/About";
-import Products from "../components/Products";
-import Contact from "../components/Contact/Contact";
-import Footer from "../components/Footer/Footer";
 
 export default function Home() {
   return (
     <div>
       <Landing />
-      <About />
-      <Products />
-      <Contact />
-      <Footer />
     </div>
   );
 }
